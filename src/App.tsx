e7b6cc4b585f6155d5b11ff321b9c60ca5dc01bd
Trevor Green <git@trumpi.co.za>
2021-10-31 15:56:45 -0400

    Reformat using Prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { FunctionComponent } from 'react';
-import Header from './common/header/index';
-import Chat from './features/chat/Chat';
-import Video from './features/video/Video';
+import React, { FunctionComponent } from "react";
+import Header from "./common/header/index";
+import Chat from "./features/chat/Chat";
+import Video from "./features/video/Video";
 
-const App: FunctionComponent = () =>  (
-    <div className="m-0 p-0 h-100 d-md-flex flex-column">
-        <Header />
-        <div className="container-fluid d-flex h-100 flex-column">
-            <div className="row flex-fill d-flex">
-                <Video />
-                <Chat />
-            </div>
-        </div>
+const App: FunctionComponent = () => (
+  <div className="m-0 p-0 h-100 d-md-flex flex-column">
+    <Header />
+    <div className="container-fluid d-flex h-100 flex-column">
+      <div className="row flex-fill d-flex">
+        <Video />
+        <Chat />
+      </div>
     </div>
+  </div>
 );
 App.displayName = "App";
 
--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,12 +1,14 @@
-import { Nav } from 'react-bootstrap';
-import { FunctionComponent } from 'react';
+import { Nav } from "react-bootstrap";
+import { FunctionComponent } from "react";
 
 const Profile: FunctionComponent = () => (
-    <Nav as="ul">
-        <Nav.Item as="li">
-            <Nav.Link href="/api/login" target="_blank">Login</Nav.Link>
-        </Nav.Item>
-    </Nav>
+  <Nav as="ul">
+    <Nav.Item as="li">
+      <Nav.Link href="/api/login" target="_blank">
+        Login
+      </Nav.Link>
+    </Nav.Item>
+  </Nav>
 );
 Profile.displayName = "Profile";
 
