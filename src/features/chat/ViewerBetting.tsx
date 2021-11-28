705015d5458b4e256d805f753990e701af7ca635
LTGPanda <jessehoebe@live.nl>
2021-11-28 19:20:47 +0100

    skeleton is done + notes
    zug zug

--- src/features/chat/ViewerBetting.tsx
+++ src/features/chat/ViewerBetting.tsx
@@ -1,10 +1,8 @@
 import { FunctionComponent } from "react";
-//import { postPresence, fetchProfile } from "../../api/api";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 
 //temp shtuff
-
 const PostAPIBett = {
   "objective": "Will Catmando become a dog?",
   "options": [
@@ -46,23 +44,59 @@ const PostAPIBettInLogged = {
   "alreadyBet": false
 };
 
-const TheBett: FunctionComponent = () => {
+/*
+docker fuckery
+react state
+axtio dispatch
+
+
+betting 
+amount o user
+
+
+POST /api/betting/{id}/bet
+        
+{
+  "amount": "200",
+  "optionId": "23"
+}
+
+response
+{
+  "id": -1,
+  "objective": "This is a test bet.",
+  "options": [
+    {
+      "description": "She will live",
+      "id": -1,
+      "total": 0
+    },
+    {
+      "description": "She will die",
+      "id": -1,
+      "total": 0
+    }
+  ],
+  "status": "Open",
+  "winningOption": null,
+  "total": 100,
+  "alreadyBet": true
+}
+*/
+
+const TheBet: FunctionComponent = () => {
   const profile = useSelector((state: RootState) => state.profile);
-  var BettList = [];
-
-  for (let index = 0; index < PostAPIBett.options.length; index++) 
-  {
-    BettList.push(<p>{PostAPIBett.options[index].description}</p>);
-  }
-
+  var BetCheck = [];
+  var BetButt = [];
+  var RadioName;
+  
   if (profile.isAuthenticated)//TODO: make api get function for betting 
   {
     if (PostAPIBettInLogged.status)//i can do this with less code but it works
     {
-      var BettCheck = [];
       for (let index = 0; index < PostAPIBett.options.length; index++) {
-        var RadioName = "Option-" + index;
-        BettCheck.push(
+        RadioName = "Option-" + index;
+        BetCheck.push(
           <div className="form-check">
             <input className="form-check-input" type="radio" name="BettOption" id={RadioName}/>
             <label className="form-check-label" htmlFor={RadioName}>
@@ -70,47 +104,63 @@ const TheBett: FunctionComponent = () => {
             </label>
         </div>
         );
-
       }
-      return(
-        <div>
-          <h5>{PostAPIBett.objective}</h5>
-          {BettCheck}
+      BetButt.push(
+        <div className="input-group mb-3 align-self-end d-flex p-3">
+          <input type='number' min="0" max={profile.balance} className="form-control"></input>
+          <div className="input-group-append">
+            <button className="btn" type="button" style={{backgroundColor: "#00db84"}}>Bet!</button>
+          </div>
         </div>
       );
     }
     else //set choosen bet active
     {
-      var BettCheck = [];
       for (let index = 0; index < PostAPIBett.options.length; index++) {
-        var RadioName = "Option-" + index;
-        BettCheck.push(
-          <div className="form-check">
-            <input className="form-check-input" type="radio" name="BettOption" id={RadioName} disabled/>
-            <label className="form-check-label" htmlFor={RadioName}>
-              {PostAPIBett.options[index].description}
-            </label>
-        </div>
-        );
+        RadioName = "Option-" + index;
 
+        //need to make sure this is correct
+        if (profile.lastTransactionId.toString() === PostAPIBettInLogged.options[index].id) 
+        {
+          BetCheck.push(
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="BettOption" id={RadioName} disabled checked/>
+              <label className="form-check-label" htmlFor={RadioName}>
+                {PostAPIBett.options[index].description}
+              </label>
+          </div>
+          );
+        }
+        else
+        {
+          BetCheck.push(
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="BettOption" id={RadioName} disabled/>
+              <label className="form-check-label" htmlFor={RadioName}>
+                {PostAPIBett.options[index].description}
+              </label>
+          </div>
+          );
+        }
       }
-      return(
-        <div>
-          <h5>{PostAPIBett.objective}</h5>
-          {BettCheck}
-        </div>
-      );
     }
   }
   else
   {
-    return(
-      <div>
-        <h5>{PostAPIBett.objective}</h5>
-        {BettList}
-      </div>
-    );
+    for (let index = 0; index < PostAPIBett.options.length; index++) 
+    {
+      BetCheck.push(<p>{PostAPIBett.options[index].description}</p>);
+    }
   }
+  return(
+    <div>
+      <div className="p-3">
+        <h5>{PostAPIBett.objective}</h5>
+        {BetCheck}
+      </div>
+      {BetButt}
+    </div>
+  );
 }
 
 
@@ -120,15 +170,17 @@ const ViewerBetting: FunctionComponent = () => {
   const twitchBGColor = {
     backgroundColor: " #18181B",
     maxWidth: 340
-  };//gör snygg og //överst char desc?  style={{height:"100%"}}
+  };
+  //gör snygg og //överst char desc?  style={{height:"100%"}}
   //https://getbootstrap.com/docs/4.0/utilities/flex/
 
   return (
       <div id="ViewerBetting" className="d-none col d-flex flex-column m-0 p-0" style={twitchBGColor}>
         <div className="d-flex p-4 border-bottom border-secondary"></div>
         <div className="d-flex justify-content-center">
-          <TheBett></TheBett>
+          <TheBet/>
         </div>
+        <div className="d-flex border-bottom border-secondary"></div>
       </div>
   );
 };
