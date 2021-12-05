53fa1b78fab0579e7313936adf54185d219511fd
Sir Rippov the Maple <6992+SirRippovMaple@users.noreply.github.com>
2021-12-04 22:56:17 -0500

    Put chat below video on small screens. (#17)

--- src/features/chat/Chat.tsx
+++ src/features/chat/Chat.tsx
@@ -3,11 +3,7 @@ import { FunctionComponent } from "react";
 const Chat: FunctionComponent = () => {
   var loc = "https://www.twitch.tv/embed/drako/chat?parent=" + window.location.hostname + "&darkpopout";
 
-  return (
-    <div className="col d-flex flex-row m-0 p-0" style={{ maxWidth: 340 }}>
-      <iframe title="Twitch Chat" src={loc} width="100%" height="100%"></iframe>
-    </div>
-  );
+  return <iframe title="Twitch Chat" src={loc} width="100%" height="100%"></iframe>;
 };
 
 Chat.displayName = "Chat";
