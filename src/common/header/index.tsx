98aea7b60b64af6bef64a710bbdfdbcfd520d655
Sir Rippov the Maple <6992+SirRippovMaple@users.noreply.github.com>
2022-01-01 23:39:09 -0500

    Collapse nav menu whenever a link is clicked. (#33)

--- src/common/header/index.tsx
+++ src/common/header/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import Profile from "../../features/profile/Profile";
@@ -9,30 +9,32 @@ const Header: FunctionComponent = () => {
   const profile = useSelector((state: RootState) => state.profile);
   const isAuthenticated = profile.isAuthenticated;
   const isModerator = profile.roles && profile.roles.includes("moderator");
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" expanded={expanded}>
       <Container fluid>
         <Navbar.Brand as={Link} to="/">
           <img src="/logo.png" alt="Drako LIVE" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-collapse" />
+        <Navbar.Toggle aria-controls="navbar-collapse" onClick={() => setExpanded(!expanded)} />
         <Navbar.Collapse id="navbar-collapse">
           <Nav className="me-auto">
-            <Nav.Link href="https://www.twitch.tv/subs/drako" target="_blank">
+            <Nav.Link href="https://www.twitch.tv/subs/drako" target="_blank" onClick={() => setExpanded(false)}>
               Subscribe
             </Nav.Link>
-            <Nav.Link as={Link} to="/leaderboard">
+            <Nav.Link as={Link} to="/leaderboard" onClick={() => setExpanded(false)}>
               Leaderboard
             </Nav.Link>
             {isAuthenticated ? (
-              <Nav.Link as={Link} to="/betting">
+              <Nav.Link as={Link} to="/betting" onClick={() => setExpanded(false)}>
                 Betting
               </Nav.Link>
             ) : (
               ""
             )}
             {isModerator ? (
-              <Nav.Link as={Link} to="/betAdmin">
+              <Nav.Link as={Link} to="/betAdmin" onClick={() => setExpanded(false)}>
                 Betting Admin
               </Nav.Link>
             ) : (
